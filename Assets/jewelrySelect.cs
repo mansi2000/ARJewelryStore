3ea5ede1e4f43813ab5272fa4d00b028fd32f75f
Mansi <mansibansal2000@gmail.com>
2020-04-21 21:42:37 +0530

    first commit

---
+++ Assets/jewelrySelect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jewelrySelect : MonoBehaviour
+{
+    public GameObject bracelet;
+    public GameObject ring;
+    public GameObject earring;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void BraceletButtonClicked(){
+        bracelet.SetActive(true);
+        ring.SetActive(false);
+        earring.SetActive(false);
+    }
+    public void RingButtonClicked(){
+        bracelet.SetActive(false);
+        ring.SetActive(true);
+        earring.SetActive(false);
+    }
+    public void EarringButtonClicked(){
+        bracelet.SetActive(false);
+        ring.SetActive(false);
+        earring.SetActive(true);
+    }
+
+    
+}
